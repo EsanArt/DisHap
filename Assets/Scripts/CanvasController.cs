91481031b63bca2bba570d0829e87c4a24cbdf75
EsanArt <emilio_1809@hotmail.com>
2020-10-22 17:11:27 -0500

    Avance Proyecto

---
+++ Assets/Scripts/CanvasController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour
+{
+    public Image imgSelector;
+    public Slider sliderBar;
+
+    public void ChangePickableBallColor(bool isSelect)
+    {
+        if (isSelect)
+        {
+            imgSelector.color = Color.red;
+        }
+        else
+        {
+            imgSelector.color = Color.white;
+        }
+    }
+
+    public void OcultarCursor(bool ocultar)
+    {
+        imgSelector.enabled = !ocultar;
+    }
+
+    public void ActivarSlider(bool activar)
+    {
+        sliderBar.gameObject.SetActive(activar);
+    }
+
+    public void SetValueBar(float vld)
+    {
+        sliderBar.value = vld;
+    }
+}
