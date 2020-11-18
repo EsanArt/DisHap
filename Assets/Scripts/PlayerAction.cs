a07e43edfb9c4cfe5c22a08240a16efa2ad738d0
Martin MisticEye <MstcEye@hotmail.com>
2020-11-17 23:21:37 -0600

    Analogo Arduino y boton implmentado

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -3,10 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO.Ports;
 
 public class PlayerAction : MonoBehaviour
 {
-
     public GameObject ball;
     SphereCollider ballCollider;
     TrailRenderer ballTrail;
@@ -16,17 +16,20 @@ public class PlayerAction : MonoBehaviour
     public float ballForceMax = 100000f;
     public float ballForce;
     public float totalTimer = 3f;
-    float currentTime = 0.0f;
+    
+    //float currentTime = 0.0f;
 
 
     public bool holdingBall = true;
     Rigidbody ballRB;
 
-    //
+    
     bool isPickableBall = false;
     public CanvasController canvasScript;
     public LayerMask pickableLayer;
     RaycastHit hit;
+
+    string[] datoBoton;
 
     // Start is called before the first frame update
     void Start()
@@ -39,75 +42,70 @@ public class PlayerAction : MonoBehaviour
         canvasScript.OcultarCursor(true);
         canvasScript.ActivarSlider(false);
         ballCollider.enabled = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (holdingBall == true)
-        {
+        datoBoton = PlayerMovement.valores;
+        int boton = int.Parse(datoBoton[2]);
 
-            if (Input.GetMouseButtonDown(0))
+            if (holdingBall == true)
             {
-                currentTime = 0.0f;
-                canvasScript.SetValueBar(0);
-                canvasScript.ActivarSlider(true);
-            }
 
-            if (Input.GetMouseButton(0))
-            {
-                currentTime += Time.deltaTime;
-                ballForce = Mathf.Lerp(ballForceMin, ballForceMax, currentTime / totalTimer);
-                canvasScript.SetValueBar(currentTime / totalTimer);
-            }
-
-            if (Input.GetMouseButtonUp(0))
-            {
-                holdingBall = false;
-                ballCollider.enabled = true;
-                ballRB.useGravity = true;
-                ballRB.AddForce(cam.forward * ballForce);
-                canvasScript.OcultarCursor(false);
-                canvasScript.ActivarSlider(false);
-                ballTrail.enabled = true;
-            }
-        }
-        else
-        {
-            if (Physics.Raycast(cam.position, cam.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, pickableLayer))
-            {
-                if (isPickableBall == false)
+                if (boton == 1 || Input.GetMouseButton(0))
                 {
-                    isPickableBall = true;
-                    canvasScript.ChangePickableBallColor(true);
-                }
-                if (isPickableBall && Input.GetKeyDown(KeyCode.E))
-                {
-                    holdingBall = true;
-                    ballCollider.enabled = false;
-                    ballRB.useGravity = false;
-                    ballRB.velocity = Vector3.zero;
-                    ballRB.angularVelocity = Vector3.zero;
-                    ball.transform.localRotation = Quaternion.identity;
+                    //currentTime = 0.0f;
+                    canvasScript.SetValueBar(0);
+                    canvasScript.ActivarSlider(true);
 
-                    GameController.instance.canScore = false;
-
-                    canvasScript.ChangePickableBallColor(true);
-                    canvasScript.OcultarCursor(true);
-                    ballTrail.enabled = false;
+                    holdingBall = false;
+                    ballCollider.enabled = true;
+                    ballRB.useGravity = true;
+                    ballRB.AddForce(cam.forward * 400);
+                    canvasScript.OcultarCursor(false);
+                    canvasScript.ActivarSlider(false);
+                    ballTrail.enabled = true;
                 }
             }
-            else if (isPickableBall == true)
+            else
             {
-                isPickableBall = false;
-                canvasScript.ChangePickableBallColor(false);
+                if (Physics.Raycast(cam.position, cam.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, pickableLayer))
+                {
+                    if (isPickableBall == false)
+                    {
+                        isPickableBall = true;
+                        canvasScript.ChangePickableBallColor(true);
+                    }
+                    if (isPickableBall && Input.GetKeyDown(KeyCode.E))
+                    {
+                        holdingBall = true;
+                        ballCollider.enabled = false;
+                        ballRB.useGravity = false;
+                        ballRB.velocity = Vector3.zero;
+                        ballRB.angularVelocity = Vector3.zero;
+                        ball.transform.localRotation = Quaternion.identity;
+
+                        GameController.instance.canScore = false;
+
+                        canvasScript.ChangePickableBallColor(true);
+                        canvasScript.OcultarCursor(true);
+                        ballTrail.enabled = false;
+                    }
+                }
+                else if (isPickableBall == true)
+                {
+                    isPickableBall = false;
+                    canvasScript.ChangePickableBallColor(false);
+                }
             }
-        }
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             SceneManager.LoadScene(0);
         }
+        print(boton);
     }
 
     private void LateUpdate()
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,14 @@ using System.Collections.Specialized;
 using System.Security.Cryptography;
 using System.Threading;
 using UnityEngine;
+using System.IO.Ports;
+using System;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public SerialPort serialPort = new SerialPort("COM3", 9600);
+
+
 
     public CharacterController controller;
     public float speed = 12f;
@@ -27,30 +32,71 @@ public class PlayerMovement : MonoBehaviour
 
     public float jumpForce = 1f;
 
+    public static string[] valores;
+
     // Update is called once per frame
+    void Start()
+    {
+        serialPort.Open();
+        serialPort.ReadTimeout = 100;
+
+    }
+
     void Update()
     {
-
         isGrounded = Physics.CheckSphere(groundCheck.position, radius, mask);
 
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = gravity;
         }
 
-        x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
-
-        move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
-
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        try
         {
-            velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
+            if (serialPort.IsOpen)
+            {
+                string s;
+
+
+                s = serialPort.ReadTo("\n");
+                valores = s.Split(' ');
+                x = int.Parse(valores[0]);
+                z = int.Parse(valores[1]);
+
+                move = transform.right * -(x-510)/1000f + transform.forward * (z-528)/1000f;
+                controller.Move(move * speed * Time.deltaTime);
+                velocity.y += gravity * Time.deltaTime;
+                controller.Move(velocity * Time.deltaTime);
+            }
+            else
+            {
+                x = Input.GetAxis("Horizontal");
+                z = Input.GetAxis("Vertical");
+
+                move = transform.right * x + transform.forward * z;
+                controller.Move(move * speed * Time.deltaTime);
+                if (Input.GetButtonDown("Jump") && isGrounded)
+                {
+                    velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
+                }
+
+                velocity.y += gravity * Time.deltaTime;
+                controller.Move(velocity * Time.deltaTime);
+            }
+
+        }
+        catch (System.Exception ex)
+        {
+            ex = new System.Exception();
         }
 
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+
+
+
+
+
+
+
         
     }
 }
